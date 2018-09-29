9c812a6f37f552048cd52d1959bb7fa9305d81ac
Viktor Afanasyev <afanasyev_vn@yahoo.co.uk>
2018-09-30 01:11:38 +0300

    Extended functionality swhich work with Array

--- js/my_first.js
+++ js/my_first.js
@@ -5,12 +5,12 @@ function addNewStudent() {
     var tblBody = document.getElementById("student");
     var row = document.createElement("tr");
 	
-	var result = {};
-	var inputs = document.getElementsByClassName('form-input');
-	for (var i = 0; i < inputs.length; i++) {
-		result[inputs[i].getAttribute('data-id')] = inputs[i].value;
-	}
-	my_students.push(result);
+    var result = {};
+    var inputs = document.getElementsByClassName('form-input');
+    for (var i = 0; i < inputs.length; i++) {
+        result[inputs[i].getAttribute('data-id')] = (inputs[i].getAttribute('data-id')!='studentLP')? inputs[i].value: ((inputs[i].checked) ?  true : false)
+    }
+    my_students.push(result);
     
     for ( var details in my_students[my_students.length-1] ) {
         var cell = document.createElement("td");
@@ -21,6 +21,11 @@ function addNewStudent() {
     tblBody.appendChild(row);
 	tbl.setAttribute("border", "1");
  }
-
-
-
+ 
+ function getStudentsList() {
+    var div_ = document.getElementById('stringStudentList');
+    div_.innerHTML =""
+    for (var student in my_students) {
+       div_.innerHTML +=`<p> ${(Object.values(my_students[student]).join("|"))}</p>`;
+    }
+ }
